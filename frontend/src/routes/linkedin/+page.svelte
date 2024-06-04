d470240145e5372d7e8a216e57d639b0b2713ff4
Dannièl Herlaar <dannielherlaar@gmail.com>
2024-06-04 23:17:50 +0200

    added filter based on skills

--- frontend/src/routes/linkedin/+page.svelte
+++ frontend/src/routes/linkedin/+page.svelte
@@ -1,10 +1,16 @@
 <script>
-  import welcome from "$lib/images/svelte-welcome.webp";
-  import welcome_fallback from "$lib/images/svelte-welcome.png";
   import { onMount } from "svelte";
   import Card from "./Card.svelte";
+  import Button from './Button.svelte';
+  import Link from './Links.svelte';
 
+  let menuOpen = false;
+  let inputValue = "";
   var apiData = [];
+  var filters = [];
+  let filteredItems = [];
+  let filteredItem = "";
+  let filteredData = [];
 
   onMount(async () => {
     try {
@@ -14,15 +20,58 @@
       }
       const data = await response.json();
       apiData = data;
+      filters.push("Alle")
+      for (let i = 0; i < apiData.length; i++) {
+        for(let j = 0; j < apiData[i].skills.length; j++){
+          filters.push(apiData[i].skills[j].skill_name);
+        }
+      }
+      console.log("filters", filters);
+      console.log("apiData", apiData);
+      filteredItems = filters; // Initially populate filteredItems with all filters
     } catch (error) {
       console.error("Fetch error:", error);
     }
   });
+
+  function selectFilter(event) {
+    filteredItem = event.detail;
+    menuOpen = false;
+    filteredData = apiData.filter((item) => item.skills.some((skill) => skill.skill_name === filteredItem));
+  }
 </script>
+
+<!-- filter based on skills -->
+<section class="dropdown">
+  <Button on:click={() => menuOpen = !menuOpen} {menuOpen} />
+	
+  <div id="myDropdown" class:show={menuOpen} class="dropdown-content">
+		{#if filteredItems.length > 0}
+			{#each filteredItems as item}
+				<Link on:selectFilter={selectFilter} link={item} />
+			{/each}
+		{:else}
+			<p>No filters found</p>
+		{/if}		
+  </div>	
+</section>
 
 <div class="flex flex-col items-center">
   <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
-    {#each apiData as item}
+    {#if filteredItem === "Alle" || filteredItem === ""}
+      {#each apiData as item}
+        <li class="">
+          <Card data={item} />
+        </li>
+      {/each}
+    {:else}
+      {#each filteredData as item}
+        <li class="">
+          <Card data={item} />
+        </li>
+      {/each}
+    {/if}
+    {#each filteredData as item}
       <li class="">
         <Card data={item} />
       </li>
@@ -30,6 +79,21 @@
   </ul>
 </div>
 
-
-<style>
+<style>		
+  .dropdown {
+    position: relative;
+    display: inline-block;
+  }
+    
+  .dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f6f6f6;
+    min-width: 230px;
+    border: 1px solid #ddd;
+    z-index: 1;
+  }
+  
+  /* Show the dropdown menu */	
+  .show {display:block;}	
 </style>
---
+++ frontend/src/routes/linkedin/Links.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+	export let link;
+  function selectFilter() {
+    dispatch("selectFilter", link);
+    }
+</script>
+
+<button on:click={selectFilter} class="black px-[12px] py-[16px] block hover:bg-[#ddd] w-full justify-start text-left">{link}</button>
